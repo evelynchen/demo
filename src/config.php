7efe400b7d1067b5899d1794033c3ecb87b61239
evelynchen <noweya@gmail.com>
2016-07-09 16:00:04 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,13 +1,13 @@
 <?php
 //DB config
-define('DBHost', '');
-define('DBUser', '');
-define('DBPass', '');
+define('DBHost', 'demo.cc17hz28exua.us-west-2.rds.amazonaws.com');
+define('DBUser', 'demo');
+define('DBPass', 'demodemo');
 
 //AWS Key config
 //define('AWSKey', '');
 //define('AWSSecret', '');
-define('AWSRegion', '');
+define('AWSRegion', 'us-west-2');
 
 //AWS S3 config
-define('S3Bucket', '');
+define('S3Bucket', 'demo-eva123');
